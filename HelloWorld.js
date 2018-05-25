430c1168ba234963bfa841d6a6d8ead45cd5ae69
Jomar Soto <alphailluminas@gmail.com>
2018-05-25 18:55:58 -0400

    Added basic routing for an About page and 404 page.

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,8 +1,21 @@
 var http = require('http')
 
 http.createServer(function(req,res){
-  res.writeHead(200, { 'Content-Type': 'text/plain'});
-  res.end('Hello World');
+  // normalize url by removing querystring, optional
+  // trailing slash, and making it lowercase
+  var path = req.url.replace(/\/?(?:\?.*)?$/,'').toLowerCase();
+  switch(path) {
+    case '':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Homepage');
+      break;
+      case '/about':
+        res.writeHead(200, {'Content-Type': 'text-plain'});
+        res.end('About');
+        break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+  }
 }).listen(3000)
 
 console.log('Server initiated on local host 3000, pres Crtl-C to terminate')
